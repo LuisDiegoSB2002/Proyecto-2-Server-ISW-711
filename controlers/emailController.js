e54a180339247a0146acb94418b2707df0b86739
LuisDiegoSB2002 <ldsb20020611@gmail.com>
2023-08-08 00:59:50 -0600

    api de email de Twilio implementada

--- controlers/emailController.js
+++ controlers/emailController.js
@@ -1,28 +1,29 @@
 const sgMail = require('@sendgrid/mail');
+
+require('dotenv').config();
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = () => {
+const sendEmail = async (req, res) => {
+  const {email} = req.body;
+  try {
 
-  const msg = {
-    to: 'ldsb20020611@gmail.com',
-    from: 'solanodiego2002@gmai.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
+    // Enviar correo de validación
+    const msg = {
+      to: 'solanodiego2002@gmail.com',
+      from: 'ldsb20020611@gmail.com',
+      subject: 'Validación de correo',
+      text: 'Su correo a sido validado con éxito.',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
 
-  return sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+    await sgMail.send(msg);
 
-module.exports = sendEmail;
+    res.status(201).json({ message: 'Usuario registrado y correo de validación enviado.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error en el servidor.' });
+  }
+}
 
-
-
-
+module.ex = sendEmail;
--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const cors = require('cors'); // Importa el módulo cors
 const app = express();
 
 const port = 3001;
-const { register, obtener, login, checkAdminRole, edit, deleteUser,createNewUser, obtenerXId } = require("./controlers/userController");
-const {Session} = require ("./controlers/sessionController");
+const { register, obtener, login, checkAdminRole, edit, deleteUser, createNewUser, obtenerXId } = require("./controlers/userController");
+const { Session } = require("./controlers/sessionController");
 //const {checkAdminRole, checkUserRole} = require ("./controlers/roleController");
-const {obtenerPrompts,createPrompts,editPrompts,deletePrompts,obtenerPromptsXId} = require("./controlers/promptsController");
+const { obtenerPrompts, createPrompts, editPrompts, deletePrompts, obtenerPromptsXId } = require("./controlers/promptsController");
 
-const sendEmail = require('./controlers/emailController'); // Importa la función
+
+
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/proyecto', {
   useNewUrlParser: true,
@@ -21,31 +25,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/proyecto', {
 app.use(express.json());
 app.use(cors()); // Usa el middleware cors
 
-app.post('/sendEmail', (req, res) => {
-  sendEmail() // Llama a la función para enviar el correo
-    .then(() => {
-      res.send('Si');
-    })
-    .catch((error) => {
-      res.status(500).send('No');
-    });
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Ruta para registro y envío de correo de validación
+app.post('/sendEmail', async (req, res) => {
+  try {
+
+    // Enviar correo de validación
+    const msg = {
+      to: 'solanodiego2002@gmail.com',
+      from: 'ldsb20020611@gmail.com',
+      subject: 'Validación de correo',
+      text: 'Su correo a sido validado con éxito.',
+      html: '<strong>Su correo a sido validado con éxito.</strong>',
+    };
+
+    await sgMail.send(msg);
+
+    res.status(201).json({ message: 'Usuario registrado y correo de validación enviado.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error en el servidor.' });
+  }
 });
+
 
 app.use(express.json());
 
 app.post('/register', register);
 
-app.post('/createNewUser',Session, createNewUser);
+app.post('/createNewUser', Session, createNewUser);
 
 app.get('/obtener', obtener);
 
 app.post('/login', login);
 
-app.put('/editUser/:id', edit );
+app.put('/editUser/:id', edit);
 
-app.delete('/deleteUser/:id',Session, deleteUser );
+app.delete('/deleteUser/:id', Session, deleteUser);
 
-app.get('/deleteUser/:id', Session, deleteUser );
+app.get('/deleteUser/:id', Session, deleteUser);
 
 app.get('/obtenerXId/:id', obtenerXId);
 
@@ -53,9 +74,9 @@ app.get('/obtenerXId/:id', obtenerXId);
 
 //Request de los Prompts
 app.get('/obtenerPrompts', obtenerPrompts);
-app.post('/createPrompts',Session, createPrompts); 
-app.patch('/editPrompts/:id',Session, editPrompts);
-app.delete('/deletePrompts/:id',Session, deletePrompts);
+app.post('/createPrompts', Session, createPrompts);
+app.patch('/editPrompts/:id', Session, editPrompts);
+app.delete('/deletePrompts/:id', Session, deletePrompts);
 app.get('/obtenerPromptsXId/:id', obtenerPromptsXId);
 
 
